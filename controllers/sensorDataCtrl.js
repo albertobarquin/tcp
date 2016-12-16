d8c8739db5d8a1becc92fdb18e4db020ad82d5cc
alberto barquin <alberto@barquin.es>
2016-12-16 07:07:01 +0100

    primera versión del REST funcionando

--- controllers/sensorDataCtrl.js
+++ controllers/sensorDataCtrl.js
@@ -1,3 +1,55 @@
 /**
  * Created by albertobarquin on 15/12/16.
  */
+
+
+"use strict";
+
+const SensorData =require('../models/sensorsData')
+
+//geters
+function getSclusterData (req, res){
+    let scId = req.params.scId;
+    var query =SensorData.find({'sc_id':scId}).
+        where('time').gt(1481790122).
+        select({'time': 1, 'clino1':1,'clino2':1, 'clino3':1, 'tempExt':1}).
+        exec((err, sensorData) => {
+            if (err) return res.status(500).send({mesagge: 'Error: '+err})
+                if (!sensorData) return res.status(404).send({mesagge: 'Error: '+err})
+
+                res.status(200).send({sensorData})
+            });
+/**
+    SensorData.find(scId,(err, sensorData) => {
+        if (err) return res.status(500).send({mesagge: 'Error: '+err})
+        if (!sensorData) return res.status(404).send({mesagge: 'Error: '+err})
+
+        res.status(200).send({scluster})
+     })
+**/
+}
+function getSclusterDataByTime (req, res){
+    let scId = req.params.scId;
+    let fromT = req.params.fromT;
+    let toT = req.params.toT;
+    var query =SensorData.find({'sc_id':scId}).
+    where('time').gt(fromT).lt(toT).
+
+    select({'time': 1, 'clino1':1,'clino2':1, 'clino3':1, 'tempExt':1}).
+    exec((err, sensorData) => {
+        if (err) return res.status(500).send({mesagge: 'Error: '+err})
+        if (!sensorData) return res.status(404).send({mesagge: 'Error: '+err})
+
+        res.status(200).send({sensorData})
+    });
+}
+
+//seters
+
+
+
+module.exports = {
+    getSclusterData,
+    getSclusterDataByTime
+
+}
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,19 @@
 const express = require('express');
 const api = express.Router();
 const SclusterCtrl = require('../controllers/sclusterCtrl');
-
+const SensorDataCtrl = require('../controllers/sensorDataCtrl');
 
 api.get('/scs',SclusterCtrl.getSclusters );
-api.get('/sc/:scId',SclusterCtrl.getSclusterData);
-api.post('/sc', SclusterCtrl.addScluster);
-api.put('/sc/:scId', SclusterCtrl.updateCluster);
-api.get('/sc/:scId/data', SclusterCtrl.getSclusterData);
-api.get('/sc/:scId/data/from/:fromTime/to/:toTime',SclusterCtrl.getSclusterDataByTime );
+//api.get('/sc/:scId',SclusterCtrl.getSclusterData);
+//api.post('/sc', SclusterCtrl.addScluster);
+//api.put('/sc/:scId', SclusterCtrl.updateCluster);
+
+api.get('/sc/:scId/data', SensorDataCtrl.getSclusterData);
+api.get('/sc/:scId/data/from/:fromT/to/:toT',SensorDataCtrl.getSclusterDataByTime );
 api.get('/sc/:scId/data/last/:lastN', SclusterCtrl.getLastSclusterData);
-api.get('/sc/:scId/sensor/:sensorID/data',SclusterCtrl.getDataBySensor);
-api.get('/sc/:scId/sensor/:sensorID/from/:fromTime/to/:toTime', SclusterCtrl.getDataBySensorByTime);
-api.get('/sc/:scId/sensor/:sensorID/last/:lastN',SclusterCtrl.getLastDataBySensor);
+
+//api.get('/sc/:scId/sensor/:sensorID/data',SclusterCtrl.getDataBySensor);
+//api.get('/sc/:scId/sensor/:sensorID/from/:fromTime/to/:toTime', SclusterCtrl.getDataBySensorByTime);
+//api.get('/sc/:scId/sensor/:sensorID/last/:lastN',SclusterCtrl.getLastDataBySensor);
 
 module.exports = api;
