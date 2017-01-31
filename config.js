33465e68bccdbc5b4b8ea7b9616b6212e79c2045
alberto barquin <alberto@barquin.es>
2017-01-31 12:53:01 +0100

    Ajustes de configuracion

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     port : process.env.REST_PORT || 3000,
     tcpPort: process.env.TCP_PORT || 55893,
     tcpAdress: "0.0.0.0",
-    db : process.env.DB || 'mongodb://127.0.0.1:27017/cloudsystratec', //'mongodb://username:password@host:port/database?options...'); 'mongodb://JohnL:3557-7334-BBbb#-BDCAP@127.0.0.1:27017/cloudapp'
+    db : process.env.DB || 'mongodb://127.0.0.1:27017/cloudsystratec', //'mongodb://username:password@host:port/database?options...'); 'mongodb://JohnL:lodesiempre-BDCAP@127.0.0.1:27017/cloudapp'
     tcpTimeOut : 60*1000,
     sampleTime : 1,
     syncCount : 10
